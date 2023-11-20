535eee42cdd6a06de20bc6735ea83fa713fcb058
saretta118 <sflora23@fed.idserve.net>
2023-11-20 21:07:29 +0100

    inserted array and ForEach in

---
+++ Notes demo/Notes demo/Note.swift
@@ -0,0 +1,8 @@
+//
+//  Note.swift
+//  Notes demo
+//
+//  Created by Sara Flora on 20/11/23.
+//
+
+import Foundation
---
+++ Notes demo/Notes demo/NoteTabView.swift
@@ -0,0 +1,18 @@
+//
+//  NoteTabView.swift
+//  Notes demo
+//
+//  Created by Sara Flora on 20/11/23.
+//
+
+import SwiftUI
+
+struct NoteTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteTabView()
+}
